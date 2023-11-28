527aa5ff74165cab3cd2135a71376e3a017964c6
redek2 <128227165+redek2@users.noreply.github.com>
2023-11-28 18:36:52 +0100

    Add files via upload

---
+++ Zadanie 4/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        List<Pracownik> lista = new List<Pracownik>();
+        lista.Add(new Sekretarka());
+        lista.Add(new Ochroniarz());
+        lista.Add(new Ksiegowy());
+
+        foreach (var i in lista)
+        {
+            i.Pracuj();
+        }
+
+        Console.ReadKey();
+    }
+
+
+    public class Pracownik
+    {
+        virtual public void Pracuj() { Console.WriteLine("Pracownik pracuje"); }
+    }
+
+    public class Sekretarka : Pracownik
+    {
+        public override void Pracuj() { Console.WriteLine("Sekretarka pracuje"); }
+    }
+
+    public class Ochroniarz : Pracownik
+    {
+        public override void Pracuj() { Console.WriteLine("Ochroniarz pracuje"); }
+    }
+
+    public class Ksiegowy : Pracownik
+    {
+        public override void Pracuj() { Console.WriteLine("Księgowy pracuje"); }
+    }
+}
---
+++ Zadanie 5/Program.cs
@@ -0,0 +1,41 @@
+﻿public interface ITypSamolotu
+{
+    void WyswietlDane();
+}
+
+public class Samolot : ITypSamolotu
+{
+    double waga;
+    int silnik;
+    int iloscSilnikow;
+    int iloscMiejsc;
+    string typUsterzenia;
+
+    public Samolot(double waga, int silnik, int iloscSilnikow, int iloscMiejsc, string typUsterzenia)
+    {
+        this.waga = waga;
+        this.silnik = silnik;
+        this.iloscSilnikow = iloscSilnikow;
+        this.iloscMiejsc = iloscMiejsc;
+        this.typUsterzenia = typUsterzenia;
+    }
+
+    public void WyswietlDane()
+    {
+        Console.WriteLine($"Waga: {waga}kg");
+        Console.WriteLine($"Ilość cylindrów w silniku: {silnik}");
+        Console.WriteLine($"Ilość silników: {iloscSilnikow}");
+        Console.WriteLine($"Ilość miejsc: {iloscMiejsc}");
+        Console.WriteLine($"Typ usterzenia: {typUsterzenia}");
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Samolot samolot = new Samolot(5000, 16, 2, 120, "centropłat");
+            Console.WriteLine("Dane samolotu:");
+            samolot.WyswietlDane();
+        }
+    }
+}

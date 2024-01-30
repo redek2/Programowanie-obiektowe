078354ca3b43df338afa26bfc487460f475eca57
redek2 <128227165+redek2@users.noreply.github.com>
2024-01-30 21:14:28 +0100

    Add files via upload

---
+++ Wieża/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Podaj liczbę krążków: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Hanoi(n, 'A', 'C', 'B');
+
+        Console.ReadKey();
+    }
+
+    static void Hanoi(int n, char pjeden, char ptrzy, char pdwa)
+    {
+        if (n > 0)
+        {
+            Hanoi(n - 1, pjeden, pdwa, ptrzy);
+
+            Console.WriteLine($"Przenieś krążek {n} z {pjeden} na {ptrzy}");
+
+            Hanoi(n - 1, pdwa, ptrzy, pjeden);
+        }
+    }
+}
